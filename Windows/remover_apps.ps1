31f135fb01e27b1e6343f02862f18949f43cd6a0
MAC_Hassio <machassiot@gmail.com>
2023-12-14 14:23:14 -0300

    edit remover_apps

--- Windows/remover_apps.ps1
+++ Windows/remover_apps.ps1
@@ -1,13 +1,9 @@
 
 # Desinstalar os widgets do Windows 11
-```powershell
 winget uninstall "windows web experience pack"
-```
+
 # Desinstalar BingNews
-```powershell
 Get-AppxPackage -alluser  Microsoft.BingNews | Remove-AppxPackage
-```
+
 # Desinstalar Xbox 
-```powershell
 Get-AppxPackage -alluser *Xbox* | Remove-AppxPackage
-```
