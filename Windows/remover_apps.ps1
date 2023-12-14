d60dae7338591233a4949302f043dc3e02e7e448
MAC_Hassio <machassiot@gmail.com>
2023-12-14 13:58:57 -0300

    criado readme e remover.apps

---
+++ Windows/remover_apps.ps1
@@ -0,0 +1,7 @@
+
+# Desinstalar os widgets do Windows 11
+winget uninstall "windows web experience pack"
+# Desinstalar BingNews
+Get-AppxPackage -alluser  Microsoft.BingNews | Remove-AppxPackage
+# Desinstalar Xbox 
+Get-AppxPackage -alluser *Xbox* | Remove-AppxPackage
